c4c2ac182df6c329cba59c83949d4cc96bf77804
izemir <laruscrococat@gmail.com>
2023-02-01 14:21:29 +0400

    Mobs` health changes added

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ public class HealthBar : MonoBehaviour,IMobComponent
     [SerializeField] private GameObject Bar;
     [SerializeField] private SpriteRenderer BarImg;
     [SerializeField] private TMP_Text Text;
+    [SerializeField] private TMP_Text ChangesText;
     private float maxHP;
     private void Awake()
     {
@@ -26,11 +28,21 @@ public class HealthBar : MonoBehaviour,IMobComponent
 
     private void OnHPChange(float health, float diff)
     {
+        if (diff != 0) StartCoroutine(ShowChanges(diff));
         var frac = health / maxHP;
         Text.text = $"{health:####}/{maxHP:####}";
         BarImg.size = new Vector2(frac, BarImg.size.y);
         var pos = BarImg.transform.localPosition;
         pos.x = -(1 - frac) / 2;
         BarImg.transform.localPosition = pos;
+    }
+
+    private IEnumerator ShowChanges(float diff)
+    {
+        if (diff > 0) ChangesText.color = Color.green;
+        else ChangesText.color = Color.red;
+        ChangesText.text = diff.ToString();
+        yield return new WaitForSeconds(1);
+        ChangesText.text = "";
     }
 }
